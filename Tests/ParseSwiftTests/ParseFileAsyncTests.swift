d6512356cc1ce511a7895c04aba2ee82d6c601da
Corey Baker <coreyearleon@icloud.com>
2023-04-03 22:30:04 -0400

    lint

--- Tests/ParseSwiftTests/ParseFileAsyncTests.swift
+++ Tests/ParseSwiftTests/ParseFileAsyncTests.swift
@@ -347,6 +347,7 @@ class ParseFileAsyncTests: XCTestCase { // swiftlint:disable:this type_body_leng
         XCTAssertEqual(downloadCount2, 0)
     }
 
+    // swiftlint:disable:next function_body_length
     func testParseURLSessionDelegateUpload() async throws {
         // swiftlint:disable:next line_length
         let downloadTask = URLSession.shared.downloadTask(with: .init(fileURLWithPath: "http://localhost:1337/parse/files/applicationId/d3a37aed0672a024595b766f97133615_logo.svg"))
