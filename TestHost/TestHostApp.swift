4817c464fa87f971a2d44825ca979d1c6e46a52c
Corey Baker <coreyearleon@icloud.com>
2023-05-10 15:30:50 -0400

    modernize testing and schemes

---
+++ TestHost/TestHostApp.swift
@@ -0,0 +1,18 @@
+//
+//  TestHostApp.swift
+//  TestHost
+//
+//  Created by Corey Baker on 5/10/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TestHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
