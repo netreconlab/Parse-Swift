f5f1b5384679bdde1b61545e41a214e889246af6
Corey <coreyearleon@icloud.com>
2023-05-13 08:24:34 -0400

    ci: Remove watch build from test suite

--- Sources/ParseSwift/Objects/ParseUser.swift
+++ Sources/ParseSwift/Objects/ParseUser.swift
@@ -1503,7 +1503,7 @@ public extension Sequence where Element: ParseUser {
         } else {
             callbackQueue.async {
                 completion(.failure(ParseError(code: .otherCause,
-                                               message: "all items to fetch must be of the same class")))
+                                               message: "All items to fetch must be of the same class")))
             }
         }
     }
