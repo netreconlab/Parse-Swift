b97e50a3bb53fd8b16fa97e1962986282b37ce67
Corey Baker <coreyearleon@icloud.com>
2025-12-10 04:22:01 -0800

    revert

--- Sources/ParseSwift/Coding/ParseEncoder.swift
+++ Sources/ParseSwift/Coding/ParseEncoder.swift
@@ -1228,7 +1228,7 @@ private struct _JSONKey : CodingKey {
 // swiftlint:disable:next line_length
 // NOTE: This value is implicitly lazy and _must_ be lazy. We're compiled against the latest SDK (w/ ISO8601DateFormatter), but linked against whichever Foundation the user has. ISO8601DateFormatter might not exist, so we better not hit this code path on an older OS.
 @available(macOS 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)
-nonisolated(unsafe) private var _iso8601Formatter: ISO8601DateFormatter = {
+private var _iso8601Formatter: ISO8601DateFormatter = {
     let formatter = ISO8601DateFormatter()
     formatter.formatOptions = .withInternetDateTime
     return formatter
